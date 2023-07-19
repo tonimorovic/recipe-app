ca51e07f84f3d85a6f9f61f062d801095c07c613
tonimorovic <toni.morovic@gmail.com>
2023-07-19 12:09:10 +0200

    Removed IFFY since top level await is now available

--- packages/recipe-api/src/index.ts
+++ packages/recipe-api/src/index.ts
@@ -4,13 +4,11 @@ import { sequelize } from './sequelize.js';
 const host = process.env.HOST || 'localhost';
 const port = Number(process.env.PORT) || 4000;
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection to database has been established successfully.');
-    app.listen(port, () => console.log(`[Ready] http://${host}:${port}`));
-  } catch (err) {
-    console.error('Unable to connect to the database:', err);
-    process.exit(1);
-  }
-})();
+try {
+  await sequelize.authenticate();
+  console.log('Connection to database has been established successfully.');
+  app.listen(port, () => console.log(`[Ready] http://${host}:${port}`));
+} catch (err) {
+  console.error('Unable to connect to the database:', err);
+  process.exit(1);
+}
